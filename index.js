853b60cb53f58d079b386b40e894e03d54cafe27
Matthew Zmija <matthewzmija@Monicas-MacBook-Air.local>
2023-05-15 16:31:21 -0400

    Added tests

--- index.js
+++ index.js
@@ -1,36 +1,60 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const { text } = require("stream/consumers");
+const { Circle, Triangle, Square } = require("./lib/shapes.js");
 
-inquirer.prompt([
-  {
-    type: "input",
-    name: "text",
-    message:
-      "Please provide the text that you want displayed on the logo (Limit the text to a maximum of 3 characters)",
-    validate: (text) => text.length >= 1 && text.length <= 3,
-  },
-  {
-    type: "input",
-    name: "textColor",
-    message:
-      "Please provide a color keyword or a hexadecimal number so that the text can be displayed in that color on your logo",
-  },
-  {
-    type: "list",
-    name: "shape",
-    message:
-      "Please choose one of the three shapes that you want your logo to be",
-    choices: ["Circle", "Triangle", "Square"],
-  },
-  {
-    type: "input",
-    name: "shapeColor",
-    message:
-      "Please provide a color keyword or a hexadecimal number so that the shape can be displayed in that color on your logo",
-  },
-]);
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "text",
+      message:
+        "Please provide the text that you want displayed on the logo (Limit the text to a maximum of 3 characters)",
+      validate: (text) => text.length > 0 && text.length < 4,
+    },
+    {
+      type: "input",
+      name: "textColor",
+      message:
+        "Please provide a color keyword or a hexadecimal number so that the text can be displayed in that color on your logo",
+    },
+    {
+      type: "list",
+      name: "shape",
+      message:
+        "Please choose one of the three shapes that you want your logo to be",
+      choices: ["Circle", "Triangle", "Square"],
+    },
+    {
+      type: "input",
+      name: "shapeColor",
+      message:
+        "Please provide a color keyword or a hexadecimal number so that the shape can be displayed in that color on your logo",
+    },
+  ])
+  .then((data) => {
+    let generatedLogo = userSelectedShape(data);
+    let fileTemplate = `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200"> ${generatedLogo} </svg>`;
+    fs.writeFile("logo.svg", fileTemplate, (err) => {
+      err ? console.log(err) : console.log("Generated logo.svg");
+    });
+  });
 
-//.then block -> if statement or a switch statement which will check data.shape to see which the user selected, call the render function
-// const generatedLogo = square.render()
-// let template = <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200"> ${generatedLogo} </svg>
+function userSelectedShape(data) {
+  switch (data.shape) {
+    case "Circle":
+      const circleLogo = new Circle(data.text, data.textColor, data.shapeColor);
+      return circleLogo.render();
+
+    case "Triangle":
+      const triangleLogo = new Triangle(
+        data.text,
+        data.textColor,
+        data.shapeColor
+      );
+      return triangleLogo.render();
+
+    case "Square":
+      const squareLogo = new Square(data.text, data.textColor, data.shapeColor);
+      return squareLogo.render();
+  }
+}
